557ed8498d94327eab19be57ef467816d8795125
Marcos <marcosuster01@gmail.com>
2026-05-16 23:00:33 +0000

    add validate-email function

--- empleados.py
+++ empleados.py
@@ -1,3 +1,9 @@
 def calcular_salario_neto(salario_bruto, impuesto_pct):
     impuesto = salario_bruto * (impuesto_pct / 100)
     return round(salario_bruto - impuesto, 2)
+
+def formatear_nombre(nombre, apellido):
+    return f"{apellido.upper()}, {nombre.capitalize()}"
+
+def validar_email(email):
+    return "@" in email and "." in email.split("@")[-1]
